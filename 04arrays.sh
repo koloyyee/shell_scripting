a22fedff413cc85665db1a275c17921e591ce5ea
Loy Yee Ko <koloyyee@gmail.com>
2024-03-16 13:22:24 -0400

    weird array

--- 04arrays.sh
+++ 04arrays.sh
@@ -19,8 +19,8 @@ echo "cars indice are 0, 1, 4, 9"
 echo ${cars[1]}
 echo ${cars[1+3]} # 4
 echo "index not 0, 1, 4, 9 will result in error."
-# echo ${cars[3]} # there is index 3, it will result in error
-#cars[3]="Tesla" # also in error because no insertion
-# echo ${cars[2+3]} #5 errorr
-echo ${cars[4+5]} # 9 ok
+echo "try yourself"
+read -p "enter any number: " idx
+echo ${cars[$idx]}
+
 echo ${cars[*]} 
